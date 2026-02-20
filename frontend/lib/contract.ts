7f749e2740025e3e4c89370cfbf71835f7ea3f81
Nnamdi Okeke <onnamdialex@gmail.com>
2026-02-20 14:35:05 +0100

    :art: Define types for the p

---
+++ frontend/lib/contract.ts
@@ -0,0 +1,54 @@
+import { STACKS_TESTNET } from "@stacks/network";
+import {
+  BooleanCV,
+  cvToValue,
+  fetchCallReadOnlyFunction,
+  ListCV,
+  OptionalCV,
+  principalCV,
+  PrincipalCV,
+  TupleCV,
+  uintCV,
+  UIntCV,
+} from "@stacks/transactions";
+
+const CONTRACT_ADDRESS = "ST3PEN3P9CDM5HNDW4VKRWY5T456AK990AZRKA5E";
+const CONTRACT_NAME = "ttt";
+
+//defining types
+type GameCv = {
+  "player-one": PrincipalCV;
+  "player-two": OptionalCV<PrincipalCV>;
+  "is-player-one-turn": BooleanCV;
+  "bet-amount": UIntCV;
+  board: ListCV<UIntCV>;
+  winner: OptionalCV<PrincipalCV>;
+};
+
+export type Game = {
+  id: number;
+  "player-one": string;
+  "player-two": string | null;
+  "is-player-one-turn": boolean;
+  "bet-amount": number;
+  board: number;
+  winner: string | null;
+};
+
+export enum Move {
+  EMPTY = 0,
+  X = 1,
+  O = 2,
+}
+
+export const EMPTY_BOARD = [
+  Move.EMPTY,
+  Move.EMPTY,
+  Move.EMPTY,
+  Move.EMPTY,
+  Move.EMPTY,
+  Move.EMPTY,
+  Move.EMPTY,
+  Move.EMPTY,
+  Move.EMPTY,
+];
