2659e3c3c6cace8b486c62353d6c80b805cb21c3
Nnamdi Okeke <onnamdialex@gmail.com>
2026-02-20 17:03:50 +0100

    :sparkles: Add the getGame and getAllGame functions | still wip ...

--- frontend/lib/contract.ts
+++ frontend/lib/contract.ts
@@ -16,6 +16,7 @@ const CONTRACT_ADDRESS = "ST3PEN3P9CDM5HNDW4VKRWY5T456AK990AZRKA5E";
 const CONTRACT_NAME = "ttt";
 
 //defining types
+//gamecv represents the clarity version of the tuple gotten from the games map of the smart contract
 type GameCv = {
   "player-one": PrincipalCV;
   "player-two": OptionalCV<PrincipalCV>;
@@ -31,7 +32,7 @@ export type Game = {
   "player-two": string | null;
   "is-player-one-turn": boolean;
   "bet-amount": number;
-  board: number;
+  board: number[];
   winner: string | null;
 };
 
@@ -52,3 +53,70 @@ export const EMPTY_BOARD = [
   Move.EMPTY,
   Move.EMPTY,
 ];
+
+//function to fetch all games
+export async function getAllGames() {
+  //fetch latest-game-id from the contract
+  const latestGameIdCV = (await fetchCallReadOnlyFunction({
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: CONTRACT_NAME,
+    functionName: "get-latest-game-id",
+    functionArgs: [],
+    senderAddress: CONTRACT_ADDRESS,
+    network: STACKS_TESTNET,
+  })) as UIntCV;
+
+  //converting the uintCV to a Js/Ts number type
+  const latestGameId = parseInt(latestGameIdCV.value.toString());
+
+  //loop from 0 to latestGameId-1 and fetch the game details for each game
+  const games: Game[] = [];
+  for (let i = 0; i < latestGameId; i++) {
+    const game = await getGame(i);
+    if (game) games.push(game);
+  }
+  return games;
+}
+
+//adding the getGame function
+export async function getGame(gameId: number) {
+  //use the get-game read only function to fetch the game details for the given gameId
+  const gameDetails = await fetchCallReadOnlyFunction({
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: CONTRACT_NAME,
+    functionName: "get-game",
+    functionArgs: [uintCV(gameId)],
+    senderAddress: CONTRACT_ADDRESS,
+    network: STACKS_TESTNET,
+  });
+
+  const responseCV = gameDetails as OptionalCV<TupleCV<GameCv>>;
+
+  //if we get back a 'none' then the game does not exist and we return null
+  if (responseCV.type === "none") {
+    return null;
+  }
+
+  //if we get back a value that is not a tuple then something went wrong and we return null
+  if (responseCV.value.type !== "tuple") {
+    return null;
+  }
+
+  //if we receive a gameCV tuple, then it can be converted to a game object
+  const gameCV = responseCV.value.value;
+
+  const game: Game = {
+    id: gameId,
+    "player-one": gameCV["player-one"].value,
+    "player-two":
+      gameCV["player-two"].type === "some"
+        ? gameCV["player-two"].value.value
+        : null,
+    "is-player-one-turn": cvToValue(gameCV["is-player-one-turn"]),
+    "bet-amount": parseInt(gameCV["bet-amount"].value.toString()),
+    board: gameCV["board"].value.map((cell) => parseInt(cell.value.toString())),
+    winner:
+      gameCV["winner"].type === "some" ? gameCV["winner"].value.value : null,
+  };
+  return game;
+}
